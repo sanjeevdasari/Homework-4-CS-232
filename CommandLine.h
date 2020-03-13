f3c7e258b3af717a0b3f8427622f6b671770dee1
Sanjeev Dasari <48922933+sanjeevdasari@users.noreply.github.com>
2020-03-12 20:04:56 -0400

    Add files via upload

---
+++ CommandLine.h
@@ -0,0 +1,38 @@
+/*Created on: Mar 11, 2020
+ *Author: Sanjeev Dasari (sd47), Partner: Charles
+ *CS 232 - Homework 4
+ */
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sstream>     // inputing and outputting stream (class)
+
+using namespace std;
+
+
+class CommandLine 
+
+{              // declaring command class
+
+                                     // Public methods and variables
+public:
+	CommandLine();
+	CommandLine(istream& in);
+	char* getCommand() const;
+	int getArgCount() const;
+	char** getArgVector() const;
+	char* getArgVector(int i) const;
+	bool noAmpersand() const;
+	virtual ~CommandLine();
+
+                                     // Private variables
+private:
+	int argc;
+	char** argv;
+	vector<string> myArgv;
+	bool noAmpersand_var;
+};
+
+
